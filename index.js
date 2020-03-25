0c0eee43a10d52862227a7370d3670308fafbef1
jbb-dev <jbbouillat@yahoo.fr>
2020-03-25 15:44:39 +0100

    upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const friends = ['Kenny', 'Nadir', 'Anais', 'Johanna']
+const chalk = require('chalk');
+
+console.log(chalk.blue(friends[0]))
+console.log(chalk.red(friends[1]))
+console.log(chalk.green(friends[2]))
+console.log(chalk.magenta(friends[3]))
